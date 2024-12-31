d32b4582f8e4559e527213e57f428ab398ae19d7
steffen.waldmann <steffen.waldmann@unic.com>
2024-12-31 14:07:31 +0100

    chore: Add some sample requests

--- data/requestBuilder.go
+++ data/requestBuilder.go
@@ -6,6 +6,11 @@ func BuildRequests() []model.HttpRequest {
 	requests := []model.HttpRequest{
 		model.NewHttpRequest("GET", "https://api.chucknorris.io/jokes/random"),
 		model.NewHttpRequest("GET", "https://uselessfacts.jsph.pl/api/v2/facts/random"),
+		model.NewHttpRequest("GET", "https://techy-api.vercel.app/api/json"),
+		model.NewHttpRequest("GET", "https://corporatebs-generator.sameerkumar.website/"),
+		model.NewHttpRequest("GET", "https://whoa.onrender.com/whoas/random"),
+		model.NewHttpRequest("GET", "https://v2.jokeapi.dev/joke/Any"),
+		model.NewHttpRequest("GET", "https://official-joke-api.appspot.com/random_joke"),
 	}
 
 	return requests
